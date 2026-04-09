b2835116a5b32b2dab52827f0576da0a8720b90d
Ra-code-hub <ferahre3@gmail.com>
2026-04-08 21:08:04 -0500

    actualizar main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,18 @@
 // src/main.jsx — Punto de entrada de la aplicacion
 // ============================================================
 
-import { StrictMode }    from 'react'
 import { createRoot }    from 'react-dom/client'
 import '@/styles/global.css'
 import { AuthProvider }  from '@/context/AuthContext'
-import { CartProvider }  from '@/context/CartContext'  // Proveedor del carrito
+import { CartProvider }  from '@/context/CartContext'
 import App               from './App'
 
 const rootElement = document.getElementById('root')
 
 createRoot(rootElement).render(
-  <StrictMode>
-    <AuthProvider>
-      {/* CartProvider envuelve la app para que useCart() funcione en cualquier componente */}
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </AuthProvider>
-  </StrictMode>
+  <AuthProvider>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </AuthProvider>
 )
