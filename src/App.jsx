e3a6e5faae467b3998d494fdf40fa8cd0c95c93a
Ra-code-hub <ferahre3@gmail.com>
2026-04-08 20:52:15 -0500

    quitar ruta protegida

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ const router = createBrowserRouter([
       // ======================================================
       {
         path: 'admin',
-        element: <RutaProtegida rol="admin"><Admin /></RutaProtegida>,
+        element: <Admin />,  // Sin RutaProtegida temporalmente
       },
       // Proximas rutas de admin:
       // { path: 'admin/pedidos',    element: <RutaProtegida rol="admin"><AdminPedidos /></RutaProtegida> },
